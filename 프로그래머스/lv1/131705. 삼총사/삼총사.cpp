07efe10d0a919497d1cdf242caf4de74f689d252
Joonhyok Choi <99248506+TerryChoi-M@users.noreply.github.com>
2023-08-14 00:32:52 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> number) {
+    int answer = 0;
+    for (int i = 0;i<number.size()-2;i++)
+    {
+        for (int j = i+1;j< number.size()-1;j++)
+        {
+            for(int k = j+1; k<number.size();k++)
+            {
+                int num = number[i] + number[j] + number[k];
+                if(num == 0)
+                {
+                    answer++;
+                }
+            }
+        }
+    }
+    return answer;
+}
